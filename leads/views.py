b2ac30c0599b5e76110a0a6ae72e2cb84ea94120
Kshitij Tyagi <kshitijtyagiwastaken@gmail.com>
2024-09-10 11:54:05 +0530

    created basic views and url paths

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from leads.models import Lead
 
-# Create your views here.
+def leads_list(request):
+    leads = Lead.objects.all()
+    context = {
+        "leads" : leads
+    }
+    return render(request, "leads/leads_list.html", context)
