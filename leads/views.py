697e55e736aac4cffc281af4d04e2f99672984c6
Kshitij Tyagi <kshitijtyagiwastaken@gmail.com>
2024-09-12 08:55:24 +0530

    Update using ModelForm

--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from leads.models import Lead, Agent
-from leads.forms import LeadForm
+from leads.forms import LeadModelForm, LeadForm
+
 
 def leads_list(request):
     leads = Lead.objects.all()
@@ -9,6 +10,7 @@ def leads_list(request):
     }
     return render(request, "leads/lead_list.html", context)
 
+
 def lead_detail(request, pk):
     lead = Lead.objects.get(id=pk)
     context = {
@@ -16,25 +18,37 @@ def lead_detail(request, pk):
     }
     return render(request, "leads/lead_detail.html", context)
 
+
 def lead_create(request):
-    form = LeadForm()
+    form = LeadModelForm()
     if request.method == 'POST':
-        form = LeadForm(request.POST)
+        form = LeadModelForm(request.POST)
         if form.is_valid():
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            age = form.cleaned_data['age']
-            agent = Agent.objects.first()
-            Lead.objects.create(
-                first_name = first_name,
-                last_name = last_name,
-                age = age,
-                agent = agent
-            )
+            form.save()
             return redirect("/leads")
 
     context = {
         'form' : form
     }
-
     return render(request, "leads/lead_create.html", context)
+
+
+def lead_update(request, pk):
+    lead = Lead.objects.get(id=pk)
+    form = LeadModelForm(instance=lead)
+    if request.method == 'POST':
+        form = LeadModelForm(request.POST, instance=lead)
+        if form.is_valid():
+            form.save()
+            return redirect("/leads")
+        
+    context = {
+        'lead' : lead,
+        'form' : form
+    }
+    return render(request, "leads/lead_update.html", context)
+
+def lead_delete(request, pk):
+    lead = Lead.objects.get(id=pk)
+    lead.delete()
+    return redirect("/leads")
